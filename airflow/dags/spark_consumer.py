4385454ef72e899a9dc4ee58e237521336a636e0
train user <train@trainvm.vbo.local>
2025-04-17 23:10:57 +0300

    kafka + spark streaming ok with enriched dataframe

--- airflow/dags/spark_consumer.py
+++ airflow/dags/spark_consumer.py
@@ -1,17 +1,14 @@
-#airflow/dags/spark_consumer.py
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StringType, DoubleType, LongType
-import os
+from pyspark.sql.types import *
 
-# Spark session
 spark = SparkSession.builder \
     .appName("EnrichRealtimeStockData") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# Kafka'dan veri oku
+# Kafka'dan oku
 df_raw = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
@@ -19,40 +16,38 @@ df_raw = spark.readStream \
     .option("startingOffsets", "latest") \
     .load()
 
-# JSON semasi
-schema = StructType() \
-    .add("symbol", StringType()) \
-    .add("price", DoubleType()) \
-    .add("timestamp", LongType())
+# Yeni JSON schema
+schema = StructType([
+    StructField("symbol", StringType()),
+    StructField("timestamp", LongType()),
+    StructField("price", DoubleType()),
+    StructField("high_52week", DoubleType()),
+    StructField("low_52week", DoubleType()),
+    StructField("volume", DoubleType()),
+    StructField("name", StringType()),
+    StructField("country", StringType()),
+    StructField("exchange", StringType()),
+    StructField("finnhubIndustry", StringType()),
+    StructField("marketCapitalization", DoubleType()),
+    StructField("weburl", StringType()),
+    StructField("logo", StringType()),
+    StructField("peRatio", DoubleType()),
+    StructField("pbRatio", DoubleType()),
+    StructField("dividendYield", DoubleType()),
+    StructField("beta", DoubleType()),
+    StructField("eps", DoubleType()),
+    StructField("debtToEquity", DoubleType()),
+    StructField("rsi", DoubleType())
+])
 
-# JSON'u parse et
+# JSON'u ayristir
 df_parsed = df_raw.selectExpr("CAST(value AS STRING) as json_str") \
     .select(from_json(col("json_str"), schema).alias("data")) \
     .select("data.*") \
     .dropna(subset=["symbol", "price", "timestamp"])
 
-# Metadata dosyasi yolu (mutlaka container icindeki mount path)
-metadata_path = "/opt/bitnami/spark/data/stock_metadata.parquet"
-
-# metadata dosyasini sadece varsa oku
-if os.path.exists(metadata_path):
-    metadata_df = spark.read.parquet(metadata_path) \
-        .dropna(subset=["symbol"]) \
-        .dropDuplicates(["symbol"])
-else:
-    # Bos bir DataFrame yarat (sema uyusmali)
-    metadata_df = spark.createDataFrame([], schema=StructType().add("symbol", StringType()) \
-                                                           .add("displaySymbol", StringType()) \
-                                                           .add("description", StringType()) \
-                                                           .add("type", StringType()) \
-                                                           .add("currency", StringType()) \
-                                                           .add("mic", StringType()))
-
-# join ile enrich
-df_enriched = df_parsed.join(metadata_df, on="symbol", how="left")
-
-# ciktiyi yaz
-query = df_enriched.writeStream \
+# Konsola yaz
+query = df_parsed.writeStream \
     .outputMode("append") \
     .format("console") \
     .option("truncate", False) \
